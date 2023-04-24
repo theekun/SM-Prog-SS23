8b1d594891b0167b0f025d0e323897eeb858855f
Theerawat Förste <theeri95@yahoo.de>
2023-04-24 23:57:31 +0200

    first commit

---
+++ src/aufgabe/MainContent.java
@@ -0,0 +1,56 @@
+package aufgabe;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+
+public class MainContent extends JPanel {
+	
+	public MainContent() {
+		
+		// Hier können Sie den Inhalt Ihres Panels definieren
+		
+		// Menüleiste erstellen
+        JMenuBar menuBar = new JMenuBar();
+        this.add(menuBar);
+
+        // Menüs erstellen
+        JMenu testMenu = new JMenu("Test");
+        menuBar.add(testMenu);
+
+        JMenu dataMenu = new JMenu("Daten");
+        menuBar.add(dataMenu);
+
+        JMenu fileMenu = new JMenu("Datei");
+        menuBar.add(fileMenu);
+
+        // Menüelemente erstellen
+        JMenuItem newTest = new JMenuItem("Neuer Test");
+        testMenu.add(newTest);
+
+        JMenuItem startTest = new JMenuItem("Test starten");
+        testMenu.add(startTest);
+
+        JMenuItem readData = new JMenuItem("Daten einlesen");
+        dataMenu.add(readData);
+
+        JMenuItem saveTests = new JMenuItem("Tests speichern");
+        fileMenu.add(saveTests);
+
+        JMenuItem loadTests = new JMenuItem("Tests laden");
+        fileMenu.add(loadTests);
+
+        JMenuItem changeName = new JMenuItem("Testnamen ändern");
+        fileMenu.add(changeName);
+
+        JMenuItem printTest = new JMenuItem("Test ausdrucken");
+        fileMenu.add(printTest);
+
+        JMenuItem exitApp = new JMenuItem("Anwendung beenden");
+        fileMenu.add(exitApp);
+		
+		
+    }
+
+}
